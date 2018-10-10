bbb19e03a4c8c9ab09329ed23865481e0f0c6816
Sk. Imtiaz Ahmed <imtiaz@cefalo.com>
2018-10-11 03:20:21 +0600

    Updates

---
+++ precache-manifest.5fd7900b5d615e0de05895b0c117f50c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d89c6b949eee09c7fe9f",
+    "url": "/static/js/main.d89c6b94.chunk.js"
+  },
+  {
+    "revision": "d01b668a73593d1040d3",
+    "url": "/static/js/1.d01b668a.chunk.js"
+  },
+  {
+    "revision": "d89c6b949eee09c7fe9f",
+    "url": "/static/css/main.8b493b97.chunk.css"
+  },
+  {
+    "revision": "95b0c981c51cec294784e4d7df3f8ccd",
+    "url": "/index.html"
+  }
+];
