a0d110f1da53f60a64fb5fcb926f2142cc403aff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-06 17:34:14 +0000

    Deploy the-neighbourhood/the-neighbourhood.github.io to github.com/the-neighbourhood/the-neighbourhood.github.io.git:master

---
+++ precache-manifest.78a37884030e8df73cefbbb7ce0d5045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6512a70bb64cb44b98a8",
+    "url": "/static/css/main.6dabcc2b.chunk.css"
+  },
+  {
+    "revision": "6512a70bb64cb44b98a8",
+    "url": "/static/js/main.6512a70b.chunk.js"
+  },
+  {
+    "revision": "58f7893771a70d6316cc",
+    "url": "/static/js/1.58f78937.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "babe58ee32919700a88a6bb954139d84",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "the-neighbourhood.me/precache-manifest.ab791e0c9f9d5d841fe04fa15d74a2e7.js"
+  "/precache-manifest.78a37884030e8df73cefbbb7ce0d5045.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("the-neighbourhood.me/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
