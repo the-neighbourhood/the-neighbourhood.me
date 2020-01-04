c8f299bacbc7e5dfd36ed871574910baabb394e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-04 05:49:18 +0000

    Deploy the-neighbourhood/the-neighbourhood.github.io to github.com/the-neighbourhood/the-neighbourhood.github.io.git:master

---
+++ precache-manifest.602d4b30524137257067f9d69ac1fab8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991bb027f2e12db9773b522498eb4982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a922ca0f4cb13dfe78d6",
+    "url": "/static/css/main.49cf6a16.chunk.css"
+  },
+  {
+    "revision": "06aa9c6069f7f309ee1e",
+    "url": "/static/js/2.72c058ea.chunk.js"
+  },
+  {
+    "revision": "a922ca0f4cb13dfe78d6",
+    "url": "/static/js/main.60b72164.chunk.js"
+  },
+  {
+    "revision": "fd665917b86c8392cada",
+    "url": "/static/js/runtime-main.9b40012a.js"
+  }
+]);
